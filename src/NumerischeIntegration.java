05f5f3a8748003bf996296595d367c53df60c5da
smlz <marco@schess.ch>
2014-09-10 11:50:44 +0200

    Implementation für Trapezregel II, II und Simpsonregel

--- src/NumerischeIntegration.java
+++ src/NumerischeIntegration.java
@@ -54,9 +54,26 @@ class TrapezregelII extends NumerischeIntegration {
     public double berechneIntegral(Funktion f, double a, double b, double epsilon) {
         Funktion f_ = f.ableitung();
 
-        /* ... */
-        
-        return 0;
+        int anzahlStreifen = 1;
+        double Tn = -1;
+        double Tn1 = (b - a) / 2 * (f.anwenden(a) + f.anwenden(b)) +
+                (b - a) * (b - a) / 12.0 * (f_.anwenden(a) - f_.anwenden(b));
+
+        while (((Math.abs(Tn1 - Tn) / Math.abs(Tn1)) > epsilon)) {
+            Tn = Tn1;
+            Tn1 = 0.0;
+            anzahlStreifen = anzahlStreifen * 2;
+            double h = (b - a) / anzahlStreifen;
+            for (int i=0; i<anzahlStreifen; i++) {
+                double links = a + i * h;  // Linker Wert des Trapezes auf der X-Achse
+                double rechts = links + h; // Rechter ...
+
+                // Einzelne Trapezflächen zur aktuellen Näherung addieren
+                Tn1 += h / 2.0 * (f.anwenden(links) + f.anwenden(rechts)) +
+                        h * h / 4.0 * (f_.anwenden(links) - f_.anwenden(rechts));
+            }
+        }
+        return Tn1;
     }
 }
 
@@ -66,10 +83,27 @@ class TrapezregelII extends NumerischeIntegration {
 class TrapezregelIII extends NumerischeIntegration {
     public double berechneIntegral(Funktion f, double a, double b, double epsilon) {
         Funktion f_ = f.ableitung();
-        
-        /* ... */
-        
-        return 0;
+
+        int anzahlStreifen = 1;
+        double Tn = -1;
+        double Tn1 = (b - a) / 2 * (f.anwenden(a) + f.anwenden(b)) +
+                (b - a) * (b - a) / 12.0 * (f_.anwenden(a) - f_.anwenden(b));
+
+        while (((Math.abs(Tn1 - Tn) / Math.abs(Tn1)) > epsilon)) {
+            Tn = Tn1;
+            Tn1 = 0.0;
+            anzahlStreifen = anzahlStreifen * 2;
+            double h = (b - a) / anzahlStreifen;
+            for (int i=0; i<anzahlStreifen; i++) {
+                double links = a + i * h;  // Linker Wert des Trapezes auf der X-Achse
+                double rechts = links + h; // Rechter ...
+
+                // Einzelne Trapezflächen zur aktuellen Näherung addieren
+                Tn1 += h / 2.0 * (f.anwenden(links) + f.anwenden(rechts)) +
+                        h * h / 12.0 * (f_.anwenden(links) - f_.anwenden(rechts));
+            }
+        }
+        return Tn1;
     }
 }
 
@@ -78,7 +112,26 @@ class TrapezregelIII extends NumerischeIntegration {
  */
 class SimpsonRegel extends NumerischeIntegration {
     public double berechneIntegral(Funktion f, double a, double b, double epsilon) {
-        /* ... */
-        return 0;
+
+        int anzahlStreifen = 1;
+        double Tn = -1;
+        double Tn1 = (b - a) / 6.0 *
+                (f.anwenden(a) + 4 * f.anwenden((a + b) / 2.0) + f.anwenden(b));
+
+        while (((Math.abs(Tn1 - Tn) / Math.abs(Tn1)) > epsilon)) {
+            Tn = Tn1;
+            Tn1 = 0.0;
+            anzahlStreifen = anzahlStreifen * 2;
+            double h = (b - a) / anzahlStreifen;
+            for (int i=0; i<anzahlStreifen; i++) {
+                double links = a + i * h;  // Linker x-Wert
+                double rechts = links + h; // Rechter x-Wert
+
+                // Einzelne Flächen zur aktuellen Näherung addieren
+                Tn1 += h / 6.0 *
+                       (f.anwenden(links) + 4 * f.anwenden((links + rechts) / 2.0) + f.anwenden(rechts));
+            }
+        }
+        return Tn1;
     }
 }
